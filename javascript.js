5730c6125febd63aaf379fd5474c4f8a34e1f0ea
Baasandash0307 <bsskaa.kai123@gmail.com>
2025-01-06 11:46:05 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,25 @@
+
+var day = 10;
+
+if(day <= 1 && day <= 9){
+    console.log('ehnii 9')
+} else if(day >= 9 && day <= 18){
+    console.log('2 dahi 9')
+} else if(day >= 19 && day <= 28){
+    console.log('3 dahi 9')
+} else if(day >= 28 && day <= 37){
+    console.log('4 dahi 9')
+} else if(day >= 37 && day <= 46){
+    console.log('5 dahi 9')
+} else if(day >= 46 && day <= 55){
+    console.log('6 dahi 9')
+} else if(day >= 55 && day <= 64){
+    console.log('7 dahi 9')
+} else if(day >= 64 && day <= 73){
+    console.log('8 dahi 9')
+} else if(day >= 73 && day <= 81){
+    console.log('9 dahi 9')
+}
+
+
+
---
+++ jscode1.js
@@ -0,0 +1,361 @@
+
+
+
+// 1-100 hurtelh sondgoi too
+
+
+// for(var i = 0; i < 100 ; i++)
+//     if(i % 2 == 1){
+//         console.log(i, 'sondgoi too');
+//     }
+
+
+
+
+
+//     // 100 hurtelh toonii niilber
+
+
+// var sum = 0;
+// for(var i = 1; i <= 100; i++){
+//     sum = sum + i;
+// }    
+//     console.log('niilber', sum);
+
+
+
+
+
+//     // 100 hurtelh toonii tegsh toonii urjver
+
+// var urjver = 1;
+// for(var i = 1; i <= 100; i++){
+//     if(i % 2 == 0)
+// urjver = urjver * i;
+// }
+// console.log('urjver', urjver)
+
+
+
+
+
+///////////
+
+
+
+
+// var day = 80;
+
+// if(day <= 1 && day <= 9){
+//     console.log('ehnii 9')
+// } else if(day >= 9 && day <= 18){
+//     console.log('2 dahi 9')
+// } else if(day >= 19 && day <= 28){
+//     console.log('3 dahi 9')
+// } else if(day >= 28 && day <= 37){
+//     console.log('4 dahi 9')
+// } else if(day >= 37 && day <= 46){
+//     console.log('5 dahi 9')
+// } else if(day >= 46 && day <= 55){
+//     console.log('6 dahi 9')
+// } else if(day >= 55 && day <= 64){
+//     console.log('7 dahi 9')
+// } else if(day >= 64 && day <= 73){
+//     console.log('8 dahi 9')
+// } else if(day >= 73 && day <= 81){
+//     console.log('9 dahi 9')
+// }
+
+
+
+
+
+////////
+
+
+
+
+// function AnhniiTooOloh(too){
+//     if(too <= 1){
+//         return false
+//     }
+//     for(var i = 2 ; i < too ; i++){
+//         if(too % i === 0){
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+
+// for(var i = 2 ; i < 100 ; i++){
+//     if(AnhniiTooOloh(i)){
+//         console.log("anhnii"+i);
+//     }
+// }
+
+
+// var number = 4
+
+// if(AnhniiTooOloh(number) === true){
+//     console.log("Anhni too")
+// }
+// else{
+//     console.log("bish")
+// }
+
+
+
+
+
+
+
+
+// const animals = ['elephant', 'cat', 'bear', 'dog']
+// let newWord = ''
+
+// for (let index = 0; index < animals.length; index ++) {
+//     const value = animals[index]
+//     newWord += value[0]
+// }
+
+// console.log(newWord) // ecbdc
+
+// const myList = [4, 8, 9, 1, 3, 10]
+
+
+
+
+
+
+
+// myList.sort((param1, param2) => {
+//     return param1 - param2;
+// })
+
+// console.log(myList) // [1, 3, 4, 8, 9, 10]
+
+
+
+
+
+
+
+// function isprimenumber(a){
+//     for(i = 0; a <= i; i++){
+//         if(a % i != 0){
+//             console.log("prime")
+//         }
+//         else{
+//             a++;
+//             if(a % i == 0){
+//                 console.log("notprime")
+//             }
+//             else{
+//                 console.log("prime")
+//             }
+//         }
+//     }
+//     console.log(a)
+//     }
+
+
+////////////
+
+
+// let weekNumber = 5;
+// let weekName;
+
+// if(weekNumber == 1){
+//     weekName = 'davaa';
+// }
+// else if (weekNumber == 2){
+//     weekName = 'myagamr'
+// }
+// else if (weekNumber == 3){
+//     weekName = 'lhagva'
+// }
+// else if (weekNumber == 4){
+//     weekName = 'purev'
+// }
+// else if (weekNumber == 5){
+//     weekName = 'baasan'
+// }
+// else if (weekNumber == 6){
+//     weekName = 'byamba'
+// }
+// else if (weekNumber == 7){
+//     weekName = 'nyam'
+// }
+// console.log(weekName);
+
+
+
+/////////////
+
+
+
+    // let gender = 'male';
+    // let age = 16;
+
+    // if(gender == 'male' && age >= 18){
+    //     console.log('nasand hursen eregtei');
+    // }
+    // else{
+    //     console.log('nasand hureegu eregtei')
+    // }
+
+
+
+
+//////////////////
+
+
+    
+
+// let day = 3;
+// let dayName;
+
+// switch (day) {
+//     case 1:
+//         dayName = 'monday'
+//         break;
+//         case 2:
+//             dayName = 'tuesday'
+//             break;
+//             case 3:
+//                 dayName = 'wednesday'
+//                 break;
+//                 case 4:
+//                     dayName = 'thursday'
+//                     break;
+//                     case 5:
+//                         dayName = 'friday'
+//                         break;
+// }
+// console.log(dayName)
+
+
+
+
+
+///////////////
+
+
+
+
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+
+
+////////////////
+
+
+
+
+// let sum = 0;
+// for(let i = 1; i <= 10; i++){
+//     if(i % 3 !==0)
+//         continue;
+//     sum += i;
+// }
+// console.log(sum);
+
+
+
+///////////////////
+
+
+
+
+// for(let i = 1; i <= 100; i++){
+//     if(i % 3===0 && i % 5===0){
+//         console.log(i);
+//         break;
+//     }
+// }
+
+
+////////////////////
+
+
+
+// for(let i = 100; i > 0; i--){
+//     if(i % 3===0 && i % 5===0){
+//         console.log(i);
+//         break;
+//     }
+// }
+
+
+
+
+/////////////
+
+
+// var x = 13;
+// var result;
+// for (var i = 2; i < x; i++) {
+//     result = x % i
+//     //  console.log(i, result)
+//     if (result == 0) {
+//         console.log(false)
+//     }
+//     else {
+//         console.log(true)
+//     }
+// }
+
+
+
+
+// for(i = 1; i <= 100; i++){
+//     if(i % 2==0){
+//         console.log(i  +'bol tegsh ');
+//     }
+// }
+
+
+
+
+
+
+// let n = 2;
+// let isPrimeNumber = true;
+
+// for(i = 2; i < n; i++){
+//     if(n % i==0){
+//         isPrimeNumber = false
+//     }
+// }
+// console.log(isPrimeNumber);
+
+
+
+
+
+
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// var text = ''
+// for(i = 0; i <= 4; i++){
+//     text+='*';
+//     console.log(text);
+// }
+
+ 
+
+
+
+
+
+
